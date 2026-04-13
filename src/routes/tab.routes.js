a71d6ff1fc8f8614a405cdb01b7d06e0ec50051a
LucasFukuda2408 <luquinhasryuji@gmail.com>
2026-04-13 14:35:24 -0300

    Colocando icones nas telas perfil e cadastro

--- src/routes/tab.routes.js
+++ src/routes/tab.routes.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from '@expo/vector-icons';
 import Cadastrado from "../screens/cadastro";
 import perfil from "../screens/perfil";
 
@@ -7,8 +8,12 @@ const Tab = createBottomTabNavigator()
 export default function TabRoutes(){
     return(
         <Tab.Navigator>
-            <Tab.Screen name="cadastro" component={Cadastrado}/>
-            <Tab.Screen name="perfil" component={perfil}/>
+            <Tab.Screen name="cadastro" component={Cadastrado}
+            options={{tabBarIcon:({color,size})=><Feather name="cadastro" color={color} size={size}/>}}
+            />
+            <Tab.Screen name="perfil" component={perfil}
+            options={{tabBarIcon:({color,size})=><Feather name="perfil" color={color} size={size}/>}}
+            />
         </Tab.Navigator>
     )
 }
